ee96ab9deadba6212e0a96c2356feeafb3fae519
SergioMaturana <sergioantonio.maturana.mena@gregoriofer.com>
2024-11-20 12:25:46 +0100

    20112024

---
+++ Interfaces - Luis/Ejercicios/xaml/WpfAppEncabezadoTextBox/MiTextBox.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppEncabezadoTextBox
+{
+    /// <summary>
+    /// Lógica de interacción para MiTextBox.xaml
+    /// </summary>
+    public partial class MiTextBox : UserControl
+    {
+        public MiTextBox()
+        {
+            InitializeComponent();
+        }
+
+        // Propiedad de dependencia para longitud máxima del texto
+        public string MaxTexto
+        {
+            get { return (string)GetValue(TextoProperty); }
+            set { SetValue(TextoProperty, value); }
+        }
+        // Registro la propiedad de dependencia
+        public static readonly DependencyProperty TextoProperty =
+            DependencyProperty.Register("MaxTexto", typeof(string), typeof(MiTextBox), new PropertyMetadata(string.Empty));
+    }
+}
